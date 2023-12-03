2d5515a215484888f66ee0b6f94fb9a6796ba886
Hritik kumar <Hritikkumar0015@gmail.com>
2023-12-03 13:50:08 +0530

    using express router

--- app.js
+++ app.js
@@ -2,31 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-// app.use('/', (req, res, next) => {
-// console.log('This allows the request to continue & it always runs for all incoming request');
-//     next();
-// })
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/add-product', (req, res, next) => {
-    // to send a response
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
-});
-
-// app.get- for all incoming get request it will get trigger
-// app.post- for all incoming post request it will get trigger
-// app.put,app.delete,app.patch
-
-// Now it will only execute for incomig POST request
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-});
-
-app.use('/', (req, res, next) => {
-    // to send a response
-    res.send('<h1>Helllo from express!</h1>');
-});
+// Here the order matters
+app.use(adminRoutes);
+app.use(shopRoutes)
 
 app.listen(3000)
