e489fd15da452787bec3f330aed690e4d7615593
Hritik kumar <Hritikkumar0015@gmail.com>
2023-12-09 12:25:50 +0530

    serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 // /admin/add-product => GET Request
 router.get('/add-product', (req, res, next) => {
-    // to send a response
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
 });
 
 // /admin/add-product => POST Request
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require('path')
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     // to send a response
-    res.send('<h1>Helllo from express!</h1>');
+    // path.join- it automatically detects the OS you are running on builds the path
+    // __dirname- it points to current working directory i.e. routes folder
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router
